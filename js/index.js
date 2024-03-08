0221979a9c981308bb598ae8b54cb40e9938f708
Md Arman Ali <www.abraryasir76@gmail.com>
2024-03-08 16:38:00 +0600

    data added local storage

--- js/index.js
+++ js/index.js
@@ -1,7 +1,46 @@
 const addProduct = () =>{
-    const productName = document.getElementById('product-name');
-    const productQuantity = document.getElementById('product-quantity');
-    const product = productName.value;
-    const quantity = productQuantity.value;
-    console.log(product,quantity);
+    const productField = document.getElementById('product-name');
+    const quantityField = document.getElementById('product-quantity');
+    const product = productField.value;
+    const quantity = quantityField.value;
+    productField.value = '';
+    quantityField.value = '';
+    console.log(product, quantity);
+    displayProduct(product, quantity);
+    saveProductLocalStorage(product, quantity);
 }
+
+const displayProduct = (product, quantity) =>{
+    const ul = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText = `${product}: ${quantity}`;
+    ul.appendChild(li);
+}
+
+const getShoppingCart = () =>{
+    const storedCart = localStorage.getItem('cart');
+    let cart = {};
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductLocalStorage = (product, quantity) =>{
+    cart = getShoppingCart();
+    cart [product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    // console.log(cartStringified);
+    localStorage.setItem('cart', cartStringified);
+}
+
+const displayProductFromLocalStorage = () =>{
+    const savedCart = getShoppingCart();
+    console.log(savedCart);
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        console.log(product,quantity);
+        displayProduct(product,quantity);
+    }
+}
+displayProductFromLocalStorage();
