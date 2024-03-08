2c88fbfe5e2ca970a6aabb87c7056a1cca63f816
Md Arman Ali <www.abraryasir76@gmail.com>
2024-03-08 10:46:11 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const addProduct = () =>{
+    const productName = document.getElementById('product-name');
+    const productQuantity = document.getElementById('product-quantity');
+    const product = productName.value;
+    const quantity = productQuantity.value;
+    console.log(product,quantity);
+}
